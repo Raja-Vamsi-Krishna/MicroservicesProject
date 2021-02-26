47d8feefd7a36c41cfe60a7f945d4326deaafd01
Lenovo <Lenovo@DESKTOP-ELFENCL>
2021-02-27 03:50:07 +0530

    7.created web client to call external web services

--- movie-catlog-service/src/main/java/io/vamsilearnings/moviecatlogservice/controller/MovieCatlogResource.java
+++ movie-catlog-service/src/main/java/io/vamsilearnings/moviecatlogservice/controller/MovieCatlogResource.java
@@ -8,7 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+//import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import io.vamsilearnings.moviecatlogservice.model.CatalogItem;
 import io.vamsilearnings.moviecatlogservice.model.Movie;
@@ -18,22 +19,30 @@ import io.vamsilearnings.moviecatlogservice.model.Rating;
 @RequestMapping("/catalog")
 public class MovieCatlogResource {
 	
+//	@Autowired
+//	private RestTemplate resttemplate;
 	@Autowired
-	private RestTemplate resttemplate;
+	private WebClient.Builder webclientbuilder;
 	@RequestMapping("/{id}")
 
 	public List<CatalogItem> getCatalog(@PathVariable("id") String userId){
+		
 		
 //		RestTemplate resttemplate=new RestTemplate();
 		List<Rating> ratings=Arrays.asList(new Rating("12345",4)
                 ,new Rating("56789",5));
 
 		return ratings.stream().map(rating->{
-			Movie movie=resttemplate
-					   .getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
-			
-			
-			
+//			Movie movie=resttemplate
+//					   .getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+//			
+//		
+			Movie movie=webclientbuilder.build()
+			.get()
+			.uri("http://localhost:8082/movies/"+rating.getMovieId())
+			.retrieve()
+			.bodyToMono(Movie.class)
+			.block();
 			return new CatalogItem(movie.getName(),"movie description",rating.getRating());
 					})
 					.collect(Collectors.toList());
